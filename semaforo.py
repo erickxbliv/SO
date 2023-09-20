9e327107e649aed05198d1209792398727929c19
erickxbliv <92802590+erickxbliv@users.noreply.github.com>
2023-09-20 10:14:08 -0300

    Update semaforo.py

--- semaforo.py
+++ semaforo.py
@@ -1,1 +1,33 @@
+import threading
+import time
+import random
 
+
+numero_de_caixas = 3
+semaforo = threading.Semaphore(numero_de_caixas)
+
+
+def recurso():
+
+    semaforo.acquire()
+
+    #print(threading.current_thread().name)
+    time.sleep(random.randint(3,10))
+ 
+    semaforo.release()
+    
+
+if __name__=="__main__":
+
+    linhas = []
+
+    for i in range(30):
+      thread = threading.Thread(target=recurso)
+      linhas.append(thread)
+
+
+    for thread in linhas:
+      thread.start()
+
+    for thread in linhas:
+      thread.join()
