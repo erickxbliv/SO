f057b750963affffd15a5fb7fb38c705b79769e0
erickxbliv <92802590+erickxbliv@users.noreply.github.com>
2023-09-20 10:56:56 -0300

    Update semaforo.py

--- semaforo.py
+++ semaforo.py
@@ -2,20 +2,44 @@ import threading
 import time
 import random
 
-
 numero_de_caixas = 3
+clientes = 30
 semaforo = threading.Semaphore(numero_de_caixas)
+lock = threading.Lock()
 
+teste = [False,False,False]
+nomes = ["Joana","Jose","Felipe"]
+execucoes = [0,0,0]
+atendimento = [30]
 
 def recurso():
 
     semaforo.acquire()
 
+    lock.acquire()
+
+    guardar = 0;
+
+    for i in range(3):
+      if teste[i] == False:
+        teste[i] = True
+        guardar = i
+        break
+
+    print(nomes[guardar] + " atendeu: ", threading.current_thread().name)
+    execucoes[i] = threading.current_thread().ident
+
+    lock.release()
+
     #print(threading.current_thread().name)
     time.sleep(random.randint(3,10))
+
+    for i in range(3):
+      if execucoes[i] == threading.current_thread().ident:
+        teste[i] = False
+        break
  
     semaforo.release()
-    
 
 if __name__=="__main__":
 
